6b970c6cde6ac8002abec437da7f3b346e3f7c06
DaniiB26 <daniib1426@gmail.com>
2025-05-21 12:48:13 +0300

    Add user for price alerts

--- comparator/src/main/java/com/example/controller/PriceAlertController.java
+++ comparator/src/main/java/com/example/controller/PriceAlertController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.PriceAlert;
@@ -30,32 +31,34 @@ public class PriceAlertController {
      * The alert specifies a product name and a target price that the user wants to
      * monitor.
      *
+     * @param userId id of the user
      * @param alert the price alert to be saved
      */
     @PostMapping
-    public void addAlert(@RequestBody PriceAlert alert) {
-        alertService.saveAlert(alert);
+    public void addAlert(@RequestParam String userId, @RequestBody PriceAlert alert) {
+        alertService.saveAlert(userId, alert);
     }
 
     /**
      * Returns all products that are equal or lower than the price alert
-     * 
-     * @param allProducts the current list of all available products
+     *
+     * @param userId id of the user
      * @return a list of products that triggered one or more alerts
      */
     @GetMapping("/triggered")
-    public List<Product> getTriggeredAlerts() throws IOException {
+    public List<Product> getTriggeredAlerts(@RequestParam String userId) throws IOException {
         List<Product> allProducts = productService.getAllProducts();
-        return alertService.getTriggeredAlerts(allProducts);
+        return alertService.getTriggeredAlerts(userId, allProducts);
     }
 
     /**
      * Returns all currently saved price alerts
      * 
+     * @param userId id of the user
      * @return a list of all alerts
      */
     @GetMapping()
-    public List<PriceAlert> getAllPriceAlerts() {
-        return alertService.getAllPriceAlerts();
+    public List<PriceAlert> getAllPriceAlertsForUser(@RequestParam String userId) {
+        return alertService.getAllPriceAlertsForUser(userId);
     }
 }
--- comparator/src/main/java/com/example/service/PriceAlertService.java
+++ comparator/src/main/java/com/example/service/PriceAlertService.java
@@ -1,7 +1,9 @@
 package com.example.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -11,26 +13,30 @@ import com.example.model.Product;
 
 @Service
 public class PriceAlertService {
-    private final List<PriceAlert> alerts = new ArrayList<>();
+    private final Map<String, List<PriceAlert>> userAlerts = new HashMap<>();
 
     /**
-     * Saves a new price alert
+     * Saves a new price alert for a user
      * 
-     * @param alert the price alert that will be added to the internal list
+     * @param userId the user that wants to save the alert
+     * @param alert the price alert that will be added to the user's list
      */
-    public void saveAlert(PriceAlert alert) {
-        alerts.add(alert);
+    public void saveAlert(String userId, PriceAlert alert) {
+        userAlerts.computeIfAbsent(userId, k -> new ArrayList<>()).add(alert);
     }
 
     /**
-     * Check all saved alerts with the current list of products
+     * Check all saved alerts of a user with the current list of products
      * Returns all products that are equal or lower than the price alert
      * 
+     * @param userId the ID of the user whose alerts should be evaluated
      * @param allProducts the current list of all available products
-     * @return a list of products that triggered one or more alerts
+     * @return a list of products that triggered one or more of the user's alerts
      */
-    public List<Product> getTriggeredAlerts(List<Product> allProducts) {
+    public List<Product> getTriggeredAlerts(String userId, List<Product> allProducts) {
         List<Product> resultedProducts = new ArrayList<>();
+
+        List<PriceAlert> alerts = userAlerts.getOrDefault(userId, new ArrayList<>());
 
         for (PriceAlert alert : alerts) {
             List<Product> matchingProducts = allProducts.stream()
@@ -45,11 +51,11 @@ public class PriceAlertService {
     }
 
     /**
-     * Returns all currently saved price alerts
+     * Returns all currently saved price alerts for a user
      * 
-     * @return a list of all alerts
+     * @return a list of all alerts for a user
      */
-    public List<PriceAlert> getAllPriceAlerts() {
-        return alerts;
+    public List<PriceAlert> getAllPriceAlertsForUser(String userId) {
+        return userAlerts.getOrDefault(userId, new ArrayList<>());
     }
 }
